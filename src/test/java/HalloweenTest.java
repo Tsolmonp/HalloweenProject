4ae8bb2ed4794684bcf8411a5ec846d0f735f85c
TsolmonP <tsolp0123@gmail.com>
2020-10-31 12:12:32 -0500

    some changes

--- src/test/java/HalloweenTest.java
+++ src/test/java/HalloweenTest.java
@@ -3,7 +3,7 @@ public class HalloweenTest {
     //I am about give candy
     //are we there yet??? Hello Hello
     //its fun to drive so far
-
+    // some changes
     //there will be no treat and trick this year
     // stay home and be safe and wear mask outside
 
