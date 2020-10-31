25faea64649dd3e41a2db10e3b50db013e3c108f
TsolmonP <tsolp0123@gmail.com>
2020-10-31 12:04:56 -0500

    no treat and trick this year no treat and trick this year

--- src/test/java/HalloweenTest.java
+++ src/test/java/HalloweenTest.java
@@ -1,7 +1,12 @@
 public class HalloweenTest {
 
     //I am about give candy
+    //are we there yet??? Hello Hello
+    //its fun to drive so far
 
+    //there will be no treat and trick this year
+    // stay home and be safe and wear mask outside
 
 }
+
 
